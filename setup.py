6ce2b22aec80e5a47cf1109d0d98404a7c5b5332
sunnysavita10 <engsunny3365@gmail.com>
2023-12-28 12:46:39 +0530

    test updated

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from typing import List
 
 HYPEN_E_DOT='-e .'
 
-def get_requiremet(file_path:str)->List[str]:
+'''def get_requiremet(file_path:str)->List[str]:
     requirements = []
     with open(file_path) as f:
         requirements=f.readlines()
@@ -11,7 +11,7 @@ def get_requiremet(file_path:str)->List[str]:
         
         if HYPEN_E_DOT in requirements:
             requirements.remove(HYPEN_E_DOT)
-    return requirements
+    return requirements'''
 
    
 with open('README.md', 'r', encoding='utf-8') as f:
@@ -38,7 +38,6 @@ setup(
     },
     package_dir={"": "src"},
     packages=find_packages(where="src"),
-    install_requires=get_requiremet("requirements.txt")
     
     
 )
