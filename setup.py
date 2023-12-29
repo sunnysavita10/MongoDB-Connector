8c721e76186bc1e50b58f212a5804e91b44177bb
sunnysavita10 <engsunny3365@gmail.com>
2023-12-29 12:59:00 +0530

    code added

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
 
 __version__ = "0.0.6"
 REPO_NAME = "mongodb_connector"
-PKG_NAME= "MongoDBConnect"
+PKG_NAME= "MongoConnect"
 AUTHOR_USER_NAME = "sunnysavita10"
 AUTHOR_EMAIL = "sunny.savita@ineuron.ai"
 
